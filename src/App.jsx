97219ed7f91fe847b8f76af1ba7c5c4ad4c3f530
Mounirus <laghamounir@gmail.com>
2024-09-25 13:34:31 +0100

    skeleton config UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Generator from "./components/Generator"
+import Hero from "./components/Hero"
+import Workout from "./components/Workout"
+
 function App() {
   
 
   return (
     <>
-      
+      <Hero />
+      <Generator />
+      <Workout />
     </>
   )
 }
